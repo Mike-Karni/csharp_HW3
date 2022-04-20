b3db951aa489aa94e090b4d9267aa1639adc26fb
mike_karni <3923499@mail.ru>
2022-04-20 08:41:27 +0300

    Решил задача : нахождение дистанции между двумя точками

---
+++ DistanceBetweenTwoDot/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача21:Напишите программу,кот принимает на вход координаты двух (.) и находит расстояние между ними в 3D простр.
+Console.Write("Введите координату x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1 = ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z2 = ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+// distance = sq.root((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1)
+
+int a = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)+ (z2-z1)*(z2-z1);
+
+double d = Math.Round(Math.Sqrt(a),2);
+Console.WriteLine("Расстояние между двумя точками равно " + d);

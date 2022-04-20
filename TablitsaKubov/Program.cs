d3c43b897f319bb8cdee855cf698d36bc2793c51
mike_karni <3923499@mail.ru>
2022-04-20 18:28:46 +0300

    Решена 3я задача

---
+++ TablitsaKubov/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while(index<=b)
+{
+    Console.Write($"{Cubes(index)}  ");
+    index++;
+}
+
+int Cubes(int x)
+{
+    return x*x*x;
+}
+

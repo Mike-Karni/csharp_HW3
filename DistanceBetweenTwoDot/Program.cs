dabcc54c848a693a3525216dded029e1a43418a3
mike_karni <3923499@mail.ru>
2022-04-22 06:51:47 +0300

    Ввели функцию

--- DistanceBetweenTwoDot/Program.cs
+++ DistanceBetweenTwoDot/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Задача21:Напишите программу,кот принимает на вход координаты двух (.) и находит расстояние между ними в 3D простр.
-Console.Write("Введите координату x1 = ");
+
+/*Console.Write("Введите координату x1 = ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату y1 = ");
 int y1 = Convert.ToInt32(Console.ReadLine());
@@ -10,16 +11,24 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату y2 = ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату z2 = ");
-int z2 = Convert.ToInt32(Console.ReadLine());
-
+int z2 = Convert.ToInt32(Console.ReadLine());*/
 // distance = sq.root((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1)
+/*int a = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)+ (z2-z1)*(z2-z1);
+double d = Math.Round(Math.Sqrt(a),2);
+Console.WriteLine("Расстояние между двумя точками равно " + d);*/
 
-int a = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)+ (z2-z1)*(z2-z1);
-
+int xA = InputCoordinate("x", "A");
+int yA = InputCoordinate("y", "A");
+int zA = InputCoordinate("z", "A");
+int xB = InputCoordinate("x", "B");
+int yB = InputCoordinate("y", "B");
+int zB = InputCoordinate("z", "B");
+// Вкорячили функцию на ввод
+int InputCoordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a = (xB-xA)*(xB-xA) + (yB-yA)*(yB-yA)+ (zB-zA)*(zB-zA);
 double d = Math.Round(Math.Sqrt(a),2);
 Console.WriteLine("Расстояние между двумя точками равно " + d);
-
-int InputCoordinate( int dot)
-{
-    Console.Write($"Введите первую коордитанату точки x {dot}");
-}
